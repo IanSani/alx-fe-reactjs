d150531a3d0b19c0a31d297589916454c0b827a4
iansani <iansani259@gmail.com>
2024-08-11 19:54:19 +0300

    Added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div>
-        <WelcomeMessage/>
+      <WelcomeMessage />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
