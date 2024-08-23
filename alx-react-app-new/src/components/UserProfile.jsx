ad44cde9b678b1536d8d73d5f0ef5af40b396950
iansani <iansani259@gmail.com>
2024-08-24 00:49:01 +0300

    Added styling

---
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -0,0 +1,10 @@
+function UserProfile(props){
+    return (
+        <div  style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+            <h1 style={{ color: 'blue' }}>{props.name}</h1>
+            <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+       <    p>Bio: {props.bio}</p>
+        </div>
+    )
+}
+export default UserProfile;
