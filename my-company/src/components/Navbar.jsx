fb398f12ebef5d717f3194ee2cf4f1f2ab9dc0c7
iansani <iansani259@gmail.com>
2024-08-25 17:00:59 +0300

    Addded styling

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,20 +2,36 @@ import React from 'react'
 import { Link } from 'react-router-dom' 
 
 function Navbar() {
+    const navStyle = {
+        backgroundColor: '#333',
+        padding: '1rem',
+      };
+    
+      const ulStyle = {
+        listStyle: 'none',
+        display: 'flex',
+        gap: '1rem',
+      };
+    
+      const linkStyle = {
+        color: 'white',
+        textDecoration: 'none',
+      };
+    
   return (
-    <nav>
-        <ul>
+    <nav style={navStyle}>
+        <ul style={ulStyle}>
             <li>
-                <Link to='Home'>Home</Link>
+                <Link to='Home' style={linkStyle}>Home</Link>
             </li>
             <li>
-                <Link to='/About'>About</Link>
+                <Link to='/About' style={linkStyle}>About</Link>
             </li>
             <li>
-                <Link to='/Services'>Services</Link>
+                <Link to='/Services' style={linkStyle}>Services</Link>
             </li>
             <li>
-                <Link to='/Contact'>Contact</Link>
+                <Link to='/Contact' style={linkStyle}>Contact</Link>
             </li>
         </ul>
     </nav>
