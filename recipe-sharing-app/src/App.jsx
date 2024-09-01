5e01eb31c1255bf6badbfb2b972bc76c449a0941
iansani <iansani259@gmail.com>
2024-09-01 23:43:12 +0300

    Added functionality

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+import create from 'zustand'
+function App() {
+  
+
+  return (
+    <>
+      <RecipeList />
+      <AddRecipeForm />
+    </>
+  )
+}
+
+export default App
