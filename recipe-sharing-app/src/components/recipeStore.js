d8a2a5c320d3b0d05e87ea75adada46bd8c5ab04
iansani <iansani259@gmail.com>
2024-09-05 15:09:21 +0300

    Added store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,24 +1,27 @@
-// src/recipeStore.js
-import {create} from 'zustand';
-
-
+import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
 
+  // Add a new recipe
   addRecipe: (recipe) => set((state) => ({
     recipes: [...state.recipes, recipe],
   })),
 
+  // Update an existing recipe
   updateRecipe: (updatedRecipe) => set((state) => ({
-    recipes: state.recipes.map(recipe =>
+    recipes: state.recipes.map((recipe) =>
       recipe.id === updatedRecipe.id ? updatedRecipe : recipe
     ),
   })),
 
+  // Delete a recipe
   deleteRecipe: (recipeId) => set((state) => ({
-    recipes: state.recipes.filter(recipe => recipe.id !== recipeId),
+    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
   })),
+
+  // Set the entire recipes array
+  setRecipes: (recipes) => set({ recipes }), // Add this method
 }));
 
 export { useRecipeStore };
