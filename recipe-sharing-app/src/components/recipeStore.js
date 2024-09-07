5eb36ba227f3c1bf6cae48641b2a7e36161114eb
iansani <iansani259@gmail.com>
2024-09-07 13:51:35 +0300

    Added

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,9 @@ import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
+  
 
   // Add a new recipe
   addRecipe: (recipe) => set((state) => ({
@@ -22,6 +25,22 @@ const useRecipeStore = create((set) => ({
 
   // Set the entire recipes array
   setRecipes: (recipes) => set({ recipes }), // Add this method
+  setSearchTerm: (term) => set(state => {
+    // Set search term and trigger recipe filtering
+    state.searchTerm = term;
+    state.filterRecipes();
+  }),
+  
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe => {
+      const searchTitle = recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase());
+      const searchIngredients = recipe.ingredients.some(ingredient =>
+        ingredient.toLowerCase().includes(state.searchTerm.toLowerCase())
+      );
+      const searchTime = recipe.preparationTime && recipe.preparationTime <= state.searchTerm;
+      return searchTitle || searchIngredients || searchTime;
+    })
+  })),
 }));
 
 export { useRecipeStore };
