8fa45c52e9d74f5fc46242c0cd2b6c9f4a6662c7
iansani <iansani259@gmail.com>
2024-09-07 13:59:40 +0300

    Enhanced ui

---
+++ recipe-sharing-app/src/components/FilterBar.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { useRecipeStore } from './recipeStore';
+
+const FilterBar = () => {
+  const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
+  const [timeFilter, setTimeFilter] = useState('');
+  
+  const handleTimeFilterChange = (e) => {
+    setTimeFilter(e.target.value);
+    setSearchTerm(e.target.value); // Optionally filter by time
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Filter by time (in minutes)"
+        value={timeFilter}
+        onChange={handleTimeFilterChange}
+      />
+      {/* Add other filter options here */}
+    </div>
+  );
+};
+
+export default FilterBar;
