4cda75bff62edc4415d607548c41a0b04bbf5819
iansani <iansani259@gmail.com>
2024-09-07 13:55:02 +0300

    Added filter

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,18 +1,24 @@
-// RecipeList component
+import React from 'react';
 import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
+  const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
 
   return (
     <div>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      {filteredRecipes.length > 0 ? (
+        filteredRecipes.map((recipe, index) => (
+          <div key={index}>
+            <h2>{recipe.title}</h2>
+            <p>{recipe.ingredients.join(', ')}</p>
+            <p>Preparation Time: {recipe.preparationTime} minutes</p>
+          </div>
+        ))
+      ) : (
+        <p>No recipes found</p>
+      )}
     </div>
   );
 };
+
 export default RecipeList;
