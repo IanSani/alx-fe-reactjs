5bd83b16ccbcc2c483a10378fb1bbb6c7088b7cc
iansani <iansani259@gmail.com>
2024-09-12 15:03:20 +0300

    Components added

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,41 +1,44 @@
-
 import './App.css';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-import FavoritesList from './components/FavoriteList';  
+import FavoritesList from './components/FavoritesList';  
 import RecommendationsList from './components/RecommendationsList';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBar from './components/SearchBar'; // Import SearchBar
 import FilterBar from './components/FilterBar'; // Import FilterBar
-
-import RecipeDetails from './components/RecipeDetails'; // Import RecipeDetails if needed
+import RecipeDetails from './components/RecipeDetails'; // Import RecipeDetails
 import EditRecipeForm from './components/EditRecipeForm';
+import { useState } from 'react';
 
 function App() {
+  const [isEditing, setIsEditing] = useState(false); // State to manage editing mode
+
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route 
-            path="/" 
-            element={
-              <>
-                {/* Add SearchBar and FilterBar  here */}
-                <SearchBar />
-                <FilterBar />
-                <RecipeList />
-                <EditRecipeForm />
-                <AddRecipeForm  />
-                <FavoritesList />
-                <RecommendationsList />
-              </>
-            } 
-          />
-          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-          {/* Add other routes here */}
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route 
+          path="/" 
+          element={
+            <>
+              <SearchBar />
+              <FilterBar />
+              {isEditing ? (
+                <EditRecipeForm onClose={() => setIsEditing(false)} />
+              ) : (
+                <>
+                  <RecipeList onEdit={() => setIsEditing(true)} />
+                  <AddRecipeForm />
+                  <FavoritesList />
+                  <RecommendationsList />
+                </>
+              )}
+            </>
+          } 
+        />
+        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
   );
 }
 
