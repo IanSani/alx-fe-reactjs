709aa2a5fd9953beac448958c65af4f1eff658e5
iansani <iansani259@gmail.com>
2024-09-17 23:53:33 +0300

    Added component

--- tailwind-react-intergration/src/App.jsx
+++ tailwind-react-intergration/src/App.jsx
@@ -1,15 +1,11 @@
 
 import './App.css'
-
+import UserProfile from './components/UserProfile' 
 function App() {
   
 
   return (
-    
-       <div className="flex items-center justify-center min-h-screen bg-blue-500">
-      <h1 className="text-4xl text-white">Hello Tailwind!</h1>
-    </div>
-    
+    <UserProfile />
   )
 }
 
